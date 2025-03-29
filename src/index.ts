2c0ab88a289b6c8f34c488e4b722a3164e273e01
Данилюк Александр Николаевич <andanilyuk@mts.ru>
2025-03-29 21:28:55 +0300

    fix decorator

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,8 @@ import 'reflect-metadata';
 import { program } from 'commander';
 import { container } from './container';
 import { ConcreteCommandFactory } from './commands/command.factory';
-import { CommandRegistry } from './commands/command.registry';
-import { GreetCommand } from './commands/greet.command';
 
-
-// const commandRegistry = container.get(CommandRegistry);
-// commandRegistry.registerCommand(GreetCommand, "greet");
 const commandFactory = container.get(ConcreteCommandFactory);
-
 
 program
   .command('greet <name>')
